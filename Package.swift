806c978e2caa1fd159800efe7671996ef8f9a2aa
sjjeong <sjjeong@cubox.ai>
2025-09-15 13:28:08 +0900

    deploy1.0

---
+++ Package.swift
@@ -0,0 +1,64 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+//https://github.com/CUBOX-Co-Ltd/dlite-sdk-sample-ios
+
+// 컴파일러 버전에 따라 타겟을 다르게 정의합니다.
+#if compiler(>=6.0)
+// MARK: - Swift 6.0 (Xcode 16.0+) Toolchain
+let cameraModuleUrl = "https://github.com/CUBOX-Co-Ltd/dlite-sdk-sample-ios/releases/download/1.0.0/Cameramodule-xcode16.xcframework.zip"
+let cameraModuleChecksum = "04cfd5a14ae206f4fdedeaf14c523fde690d353566b46a2b426b318e67bb6e33"
+
+let dliteUrl = "https://github.com/CUBOX-Co-Ltd/dlite-sdk-sample-ios/releases/download/1.0.0/Dlite-xcode16.xcframework.zip"
+let dliteChecksum = "e6ec64f6739dee9796224ada0bfd1d34dfe5cc1f2ca780d0af3f19f590548f04"
+
+let inferenceUrl = "https://github.com/CUBOX-Co-Ltd/dlite-sdk-sample-ios/releases/download/1.0.0/Inferencemodule-xcode16.xcframework.zip"
+let inferenceChecksum = "991a92f7e5dcb076cc50db36e0bfee09a1a062c35a2e32c313a7079a0f6aea1b"
+
+#else
+// MARK: - Swift 5.9 (Xcode 15.x) Toolchain
+let cameraModuleUrl = "https://github.com/CUBOX-Co-Ltd/dlite-sdk-sample-ios/releases/download/1.0.0/Cameramodule-xcode15.xcframework.zip"
+let cameraModuleChecksum = "40d6b561121c00fd1a867066b73b5f02149db98a0165b2dd18c0845fee9bdf29"
+
+let dliteUrl = "https://github.com/CUBOX-Co-Ltd/dlite-sdk-sample-ios/releases/download/1.0.0/Dlite-xcode15.xcframework.zip"
+let dliteChecksum = "ee70962248d99fd35144590a9f7c4194b452d874ba4673d5e2caeac20254b24b"
+
+let inferenceUrl = "https://github.com/CUBOX-Co-Ltd/dlite-sdk-sample-ios/releases/download/1.0.0/Inferencemodule-xcode15.xcframework.zip"
+let inferenceChecksum = "6d8dc2449c9e44d8641967588026671d5ed8ff12bfb09b620980fe4a44cbd487"
+#endif
+
+let package = Package(
+    name: "dlite-sdk-sample-ios",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "Cameramodule",
+            targets: ["Cameramodule"]),
+        .library(
+            name: "Inferencemodule",
+            targets: ["Inferencemodule"]),
+        .library(
+            name: "Dlite",
+            targets: ["Dlite"])
+    ],
+    targets: [
+        // 타겟의 'name'은 동일하게 유지하고, URL과 체크섬만 변수를 사용해 설정합니다.
+        .binaryTarget(
+            name: "Cameramodule",
+            url: cameraModuleUrl,
+            checksum: cameraModuleChecksum
+        ),
+        .binaryTarget(
+            name: "Dlite",
+            url: dliteUrl,
+            checksum: dliteChecksum
+        ),
+        .binaryTarget(
+            name: "Inferencemodule",
+            url: inferenceUrl,
+            checksum: inferenceChecksum
+        )
+    ]
+)
